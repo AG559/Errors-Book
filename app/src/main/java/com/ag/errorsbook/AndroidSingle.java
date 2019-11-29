9b9bd6e6adb43f9e86d3243108b14beada7f42bd
AG559 <ag.msx938@gmail.com>
2019-11-29 17:36:40 +0630

    modified Android Single Photo view

--- app/src/main/java/com/ag/errorsbook/AndroidSingle.java
+++ app/src/main/java/com/ag/errorsbook/AndroidSingle.java
@@ -2,28 +2,39 @@ package com.ag.errorsbook;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.text.HtmlCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.ag.errorsbook.adapter.AndroidSinglePhotoAdapter;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 
 public class AndroidSingle extends AppCompatActivity {
-    RecyclerView recyclerView;
     ArrayList<Integer> list;
     Toolbar toolbar;
     TextView title, content;
+    SliderView sliderView;
+
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +45,10 @@ public class AndroidSingle extends AppCompatActivity {
         getSupportActionBar().setTitle("Error Collection Book");
 
 
-        recyclerView = findViewById(R.id.android_single_row_recycler);
         title = findViewById(R.id.asingle_tv_title);
         content = findViewById(R.id.asingle_tv_content);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
+
+
         Intent i = getIntent();
         list = i.getIntegerArrayListExtra("img_array");
 
@@ -46,8 +57,41 @@ public class AndroidSingle extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             content.setText(HtmlCompat.fromHtml(i.getStringExtra("description"), 0));
         }
-        AndroidSinglePhotoAdapter adapter = new AndroidSinglePhotoAdapter(this, list);
-        recyclerView.setAdapter(adapter);
+        sliderView = findViewById(R.id.imageSlider);
+
+        final AndroidSinglePhotoAdapter adapter = new AndroidSinglePhotoAdapter(this);
+        adapter.setCount(5);
+
+        sliderView.setSliderAdapter(adapter);
+        sliderView.setIndicatorVisibility(false);
+        sliderView.setSliderTransformAnimation(SliderAnimations.CUBEINROTATIONTRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.startAutoCycle();
+
+
+        sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
+            @Override
+            public void onIndicatorClicked(int position) {
+                sliderView.setCurrentPagePosition(position);
+            }
+        });
+
+        sliderDotspanel = findViewById(R.id.SliderDots);
+        dotscount = list.size();
+        dots = new ImageView[dotscount];
+
+        for (int ii = 0; ii < dotscount; ii++) {
+
+            dots[ii] = new ImageView(this);
+            dots[ii].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDotspanel.addView(dots[ii], params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
     }
 
     @Override
@@ -64,4 +108,8 @@ public class AndroidSingle extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
+
+
+
--- app/src/main/java/com/ag/errorsbook/adapter/AndroidSinglePhotoAdapter.java
+++ app/src/main/java/com/ag/errorsbook/adapter/AndroidSinglePhotoAdapter.java
@@ -2,51 +2,103 @@ package com.ag.errorsbook.adapter;
 
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ag.errorsbook.R;
+import com.bumptech.glide.Glide;
+import com.smarteist.autoimageslider.SliderViewAdapter;
 
-import java.util.ArrayList;
-
-public class AndroidSinglePhotoAdapter extends RecyclerView.Adapter<AndroidSinglePhotoAdapter.PhotoHolder> {
+public class AndroidSinglePhotoAdapter extends SliderViewAdapter<AndroidSinglePhotoAdapter.SliderAdapterVH> {
     private Context context;
-    private ArrayList<Integer> list;
+    private int mCount;
 
-    public AndroidSinglePhotoAdapter(Context context, ArrayList<Integer> list) {
+    public AndroidSinglePhotoAdapter(Context context) {
         this.context = context;
-        this.list = list;
     }
 
-    @NonNull
-    @Override
-    public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.android_single_row_image, parent, false);
-        return new PhotoHolder(v);
+    public void setCount(int count) {
+        this.mCount = count;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
-        holder.imageView.setImageResource(list.get(position));
+    public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
+        View inflate = LayoutInflater.from(context).inflate(R.layout.android_single_row_image, parent, false);
+        return new SliderAdapterVH(inflate);
     }
 
     @Override
-    public int getItemCount() {
-        return list.size();
-    }
+    public void onBindViewHolder(SliderAdapterVH viewHolder, final int position) {
 
-    class PhotoHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
 
-        PhotoHolder(@NonNull View itemView) {
-            super(itemView);
-            imageView = itemView.findViewById(R.id.aimg_single_row);
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This is item in position " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switch (position) {
+            case 0:
+                viewHolder.textViewDescription.setText("This is slider item " + position);
+                viewHolder.textViewDescription.setTextSize(16);
+                viewHolder.textViewDescription.setTextColor(Color.WHITE);
+                Glide.with(viewHolder.itemView)
+                        .load("https://scontent.frgn9-1.fna.fbcdn.net/v/t1.0-9/45358969_1964329763610438_3931455836890595328_n.jpg?_nc_cat=100&_nc_ohc=pMxtsAPotcQAQmJIvoisTJj1re_aohA4vweQ7OaB0aIpTJaetEvQC_aWw&_nc_ht=scontent.frgn9-1.fna&oh=4c1ca6cb12cf45f20b866a162da99ccb&oe=5E4913EB")
+                        .fitCenter()
+                        .into(viewHolder.imageViewBackground);
+                break;
+            case 1:
+                viewHolder.textViewDescription.setText("This is slider item " + position);
+                viewHolder.textViewDescription.setTextSize(16);
+                viewHolder.textViewDescription.setTextColor(Color.WHITE);
+                Glide.with(viewHolder.itemView)
+                        .load("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
+                        .fitCenter()
+                        .into(viewHolder.imageViewBackground);
+                break;
+            case 2:
+                viewHolder.textViewDescription.setText("This is slider item " + position);
+                viewHolder.textViewDescription.setTextSize(16);
+                viewHolder.textViewDescription.setTextColor(Color.WHITE);
+                Glide.with(viewHolder.itemView)
+                        .load("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+                        .fitCenter()
+                        .into(viewHolder.imageViewBackground);
+                break;
+            default:
+
+                break;
+
         }
 
     }
+
+    @Override
+    public int getCount() {
+        //slider view count could be dynamic size
+        return mCount;
+    }
+
+    class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
+
+        View itemView;
+        ImageView imageViewBackground;
+        TextView textViewDescription;
+        ImageView imagegifContainer;
+
+        SliderAdapterVH(View itemView) {
+            super(itemView);
+            imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
+            textViewDescription = itemView.findViewById(R.id.tv_auto_image_slider);
+            imagegifContainer = itemView.findViewById(R.id.iv_gif_container);
+            this.itemView = itemView;
+        }
+    }
+
 }
